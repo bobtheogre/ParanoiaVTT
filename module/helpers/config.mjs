a566af6768d4399e14b32be6593897c5f7ed3dca
Bob Huff <bobtheogre@gmail.com>
2024-06-22 21:27:48 -0500

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const PARANOIA = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+PARANOIA.abilities = {
+  str: 'PARANOIA.Ability.Str.long',
+  dex: 'PARANOIA.Ability.Dex.long',
+  con: 'PARANOIA.Ability.Con.long',
+  int: 'PARANOIA.Ability.Int.long',
+  wis: 'PARANOIA.Ability.Wis.long',
+  cha: 'PARANOIA.Ability.Cha.long',
+};
+
+PARANOIA.abilityAbbreviations = {
+  str: 'PARANOIA.Ability.Str.abbr',
+  dex: 'PARANOIA.Ability.Dex.abbr',
+  con: 'PARANOIA.Ability.Con.abbr',
+  int: 'PARANOIA.Ability.Int.abbr',
+  wis: 'PARANOIA.Ability.Wis.abbr',
+  cha: 'PARANOIA.Ability.Cha.abbr',
+};
